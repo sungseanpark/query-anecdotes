f9a6ecf8a67bc539b5fbdd452b49983c24bd2d20
sungseanpark <sungseanpark@gmail.com>
2024-03-24 20:15:46 -0700

    Complete part 6.24

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -12,7 +12,19 @@ const AnecdoteForm = () => {
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData(['anecdotes'])
       queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+      const message = `You created '${newAnecdote.content}'`
+      dispatch({type: 'set', payload: message})
+      setTimeout(() => {
+        dispatch({type: 'clear'})
+      }, 5000)
     },
+    onError: () => {
+      const message = `Anecdote too short, must have 5 characters or more`
+      dispatch({type: 'set', payload: message})
+      setTimeout(() => {
+        dispatch({type: 'clear'})
+      }, 5000)
+    }
   })
 
   const onCreate = (event) => {
@@ -20,11 +32,11 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({ content, votes: 0})
-    const message = `You created '${content}'`
-    dispatch({type: 'set', payload: message})
-    setTimeout(() => {
-      dispatch({type: 'clear'})
-    }, 5000)
+    // const message = `You created '${content}'`
+    // dispatch({type: 'set', payload: message})
+    // setTimeout(() => {
+    //   dispatch({type: 'clear'})
+    // }, 5000)
     console.log('new anecdote')
 }
 
